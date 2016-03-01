e4cbb61b53a5a9b9ec59324ee517e2b97988b739
林冰杰 <lin.bingjie@puyutech.com>
2016-03-01 21:49:55 +0800

    LBJ

---
+++ AssistantChannel/LBJViewController.m
@@ -0,0 +1,37 @@
+//
+//  LBJViewController.m
+//  AssistantChannel
+//
+//  Created by 林冰杰 on 16/3/1.
+//  Copyright © 2016年 dxw. All rights reserved.
+//
+
+#import "LBJViewController.h"
+
+@interface LBJViewController ()
+
+@end
+
+@implementation LBJViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
