b40cb0616b74421ef4312b0fe6ee18121db75f18
lin333 <972867381@qq.com>
2016-03-07 20:02:16 +0800

    visitsViewContorller

---
+++ AssistantChannel/Controller/visits/VisitsViewController.h
@@ -0,0 +1,13 @@
+//
+//  VisitsViewController.h
+//  AssistantChannel
+//
+//  Created by 林冰杰 on 16/3/7.
+//  Copyright © 2016年 dxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VisitsViewController : UIViewController
+
+@end
