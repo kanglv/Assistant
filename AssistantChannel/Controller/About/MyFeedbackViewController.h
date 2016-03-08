3333e7b53b97dba98873fc4c6e4803b4e38a2de7
张晓烨 <82387913@qq.com>
2016-03-08 15:21:31 +0800

    修改意见反馈

--- AssistantChannel/Controller/About/MyFeedbackViewController.h
+++ AssistantChannel/Controller/About/MyFeedbackViewController.h
@@ -2,11 +2,17 @@
 //  MyFeedbackViewController.h
 //  AssistantChannel
 //
-//  Created by lvkang on 16/3/3.
+//  Created by 张晓烨 on 16/3/8.
 //  Copyright © 2016年 dxw. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "ZXYBaseViewController.h"
 
 @interface MyFeedbackViewController : ZXYBaseViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *TextView;
+@property (weak, nonatomic) IBOutlet UILabel *textFileLabel;
+@property (strong, nonatomic) NSString *suggestMessage;
+@property (weak, nonatomic) IBOutlet UIButton *doSendbtn;
+
 @end
--- AssistantChannel/Controller/About/MyFeedbackViewController.m
+++ AssistantChannel/Controller/About/MyFeedbackViewController.m
@@ -2,133 +2,114 @@
 //  MyFeedbackViewController.m
 //  AssistantChannel
 //
-//  Created by lvkang on 16/3/3.
+//  Created by 张晓烨 on 16/3/8.
 //  Copyright © 2016年 dxw. All rights reserved.
 //
 
 #import "MyFeedbackViewController.h"
 
-@interface MyFeedbackViewController ()<UITextViewDelegate,UITextFieldDelegate>
-@property(strong,nonatomic)UITextField *textField;
-@property(strong,nonatomic)UITextView *feedback;
-@property(strong,nonatomic)UILabel *placeHolderLabel,*placeHolderLabel2;
-@property(strong,nonatomic)NSString *feedBackStr,*contactStr,*feedBackStr2;
+@interface MyFeedbackViewController ()<UITextViewDelegate>
+
 @end
 
 @implementation MyFeedbackViewController
 
+@synthesize TextView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title= @"意见反馈";
-    self.view.backgroundColor = [UIColor whiteColor ];
+    self.navigationItem.title = @"意见反馈";
     
-    [self initView];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
--(void)initView{
     UIButton *backBtn = [self setNaviCommonBackBtn];
     [backBtn addTarget:self action:@selector(backBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
     
-    _feedback = [[UITextView alloc] initWithFrame:CGRectMake(5, 0, self.view.frame.size.width-10, self.view.frame.size.height/3)];
-    _feedback.backgroundColor = [UIColor whiteColor];
-    _feedback.font = [UIFont fontWithName:@"Arial" size:15.0f];
-    _feedback.layer.cornerRadius = 5.0f;
-    [_feedback becomeFirstResponder];
-    _feedback.delegate = self;
-    [self.view addSubview:_feedback];
+    self.TextView.returnKeyType = UIReturnKeyDone;
     
-    _placeHolderLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 10, self.view.frame.size.width-10, 15)];
-    _placeHolderLabel.text = @"请留下您的宝贵意见";
-    _placeHolderLabel.font = [UIFont fontWithName:@"Arial" size:15.0f];
-    _placeHolderLabel.textColor = [UIColor lightGrayColor];
-    [_feedback addSubview:_placeHolderLabel];
+    self.TextView.layer.masksToBounds = YES;
+    self.TextView.layer.cornerRadius = 8;
     
-    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(5,self.feedback.frame.size.height+10, self.view.frame.size.width-10, 30)];
-    self.textField.backgroundColor = [UIColor whiteColor];
-    self.textField.multipleTouchEnabled = YES;
-    self.textField.borderStyle = UITextBorderStyleRoundedRect;//边框
-    self.textField.font = [UIFont fontWithName:@"Arial" size:15.0f];
-    self.textField.adjustsFontSizeToFitWidth = YES;//自动缩小
-    self.textField.clearButtonMode = UITextFieldViewModeWhileEditing;//一次性删除
-    self.textField.delegate = self;
-    [self.view addSubview:self.textField];
+    self.doSendbtn.layer.cornerRadius = 8;
+    self.doSendbtn.layer.masksToBounds = YES;
     
-    _placeHolderLabel2 = [[UILabel alloc]initWithFrame:CGRectMake(0, 8, self.view.frame.size.width-10, 15)];
-    _placeHolderLabel2.text = @"手机号/Email:方便我们及时给您回复";
-    _placeHolderLabel2.font = [UIFont fontWithName:@"Arial" size:15.0f];
-    _placeHolderLabel2.textColor = [UIColor lightGrayColor];
-    [self.textField addSubview:_placeHolderLabel2];
-    
-    
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width/3, self.feedback.frame.size.height+10+self.textField.frame.size.height+30, self.view.frame.size.width/3, 30)];
-    [btn setTitle:@"发送" forState:UIControlStateNormal];
-    [btn.layer setCornerRadius:5.0f];
-    [btn.layer setBorderWidth:0.5f];
-    btn.backgroundColor = [UIColor colorWithRed:135/255.0 green:206/255.0 blue:250/255.0 alpha:1];
-    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
-    [btn addTarget:self action:@selector(senderMessage) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
-}
-
--(void)textFieldDidBeginEditing:(UITextField *)textField{
-    _placeHolderLabel2.hidden = YES;
-    self.contactStr = textField.text;
-}
--(void)textFieldDidEndEditing:(UITextField *)textField{
-    if ([textField.text isEqualToString:@""]) {
-        _placeHolderLabel2.hidden = NO;
-    }else{
-        self.contactStr = textField.text;
-    }
-}
-
--(BOOL)textViewShouldBeginEditing:(UITextView *)textView{
-    _placeHolderLabel.hidden = YES;
-    _feedback.textColor = [UIColor blackColor];
-    return YES;
-}
--(BOOL)textViewShouldEndEditing:(UITextView *)textView{
-    if ([textView.text isEqualToString:@""]) {
-        _placeHolderLabel.hidden = NO;
-    }else{
-    }
-    return YES;
-}
--(void)textViewDidChange:(UITextView *)textView{
-    self.feedBackStr = textView.text;
-}
-
--(void)senderMessage{
-    if (self.feedBackStr == nil || [self.feedBackStr isEqualToString:@""]) {
-        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:@"亲,请填写建议或意见" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
-        [alert show];
-    }else{
-        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:@"发送成功,感谢您的建议和意见" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
-                    [alert show];
-    }
-}
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    [self.view endEditing:YES];
-}
-
--(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
-    if ([string isEqualToString:@"\n"]) {
-        [self.view endEditing:YES];
-        return 0;
-    }else{
-        self.contactStr = textField.text;
-    }
-    return YES;
 }
 
 - (void)backBtnClicked:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)textViewDidChange:(UITextView *)textView{
+    
+    if ([TextView.text length] == 0) {
+        [self.textFileLabel setHidden:NO];
+    }else{
+        [self.textFileLabel setHidden:YES];
+        
+    }
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView{
+    
+    self.suggestMessage = textView.text;
+}
+
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    }
+    return YES;
+}
+
+
+- (IBAction)doSendFeedback:(UIButton *)sender {
+
+    if (self.suggestMessage.length == 0) {
+        ALERT_ERR_MSG(@"请先填写您宝贵的意见");
+        return;
+    }
+    
+    
+    [self sendData];
+}
+
+- (void) sendData
+{
+    
+    CommonService *service = [[CommonService alloc] init];
+    UserEntity *userEntity = [UserEntity sharedInstance];
+    
+    NSDictionary *param = [NSDictionary dictionaryWithObjectsAndKeys:
+                           @"user",@"m",
+                           @"feedback",@"a",
+                           userEntity.sn,@"sn",
+                           self.suggestMessage,@"content",
+                           nil
+                           ];
+    
+    [service getNetWorkData:param Successed:^(id entity) {
+        
+        NSNumber *state = [entity valueForKeyPath:@"success"];
+        NSString *strState = [NSString stringWithFormat:@"%d", [state intValue]];
+        
+        
+        if ([strState isEqualToString:@"1"]) {
+
+            
+        }else{
+            
+        }
+        
+    } Failed:^(int errorCode, NSString *message) {
+        
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
